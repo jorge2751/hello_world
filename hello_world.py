046cae2b9344b7b6094662702365495620ccfc95
Jorge Leon <84067777+jorge2751@users.noreply.github.com>
2023-02-06 10:50:59 -0800

    First commit

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+#1
+print("Hello World")
+#2
+name = "Jorge Leon"
+print("Hello", name, "!")
+print("Hello " + name + "!")
+#3
+num = 71
+print("Hello", num, "!")
+print("Hello " + str(num) + "!")
+#4
+food_one = "Buffalo Wings"
+food_two = "Pizza"
+print("I love to eat {} and {}.".format(food_one, food_two))
+print(f"I love to eat {food_one} and {food_two}.")
